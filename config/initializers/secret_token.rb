b8a1ea4e941046c053dd37622a38021b3d2812dc
Amin.T <no@no.com>
2011-04-17 12:20:47 +0000

    First commint: setting up the rails files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AnnonceImmo1::Application.config.secret_token = '9cfd84797c13f49860a9305bf5a992ee61814a5efb8d06f4b5ff687e0bc2b30463f5c42a9c85865bc15440f040a9996e56d046275a4ddc07c9cc0ed134043eab'
